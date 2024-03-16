f167132d7bb820ea40c510db31b95d9c9f795a41
Maxim77553300 <81866299+Maxim77553300@users.noreply.github.com>
2024-03-16 16:19:23 +0300

    features/PROJECT-1- test teamcity15

--- src/main/java/org/example/controller/AuthenticationController.java
+++ src/main/java/org/example/controller/AuthenticationController.java
@@ -36,7 +36,7 @@ public class AuthenticationController {
     private BCryptPasswordEncoder passwordEncoder;
 
 
-    // test14
+    // test15
     @PostMapping("/users")
     @ResponseStatus(HttpStatus.OK)
     public void save(@Valid @RequestBody User userDto) {
