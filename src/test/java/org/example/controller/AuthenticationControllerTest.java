9cca995c6082b40da212bec09ab5811de858e8a9
Maxim77553300 <81866299+Maxim77553300@users.noreply.github.com>
2024-03-18 18:27:31 +0300

    features/PROJECT-1- test teamcity with test example

---
+++ src/test/java/org/example/controller/AuthenticationControllerTest.java
@@ -0,0 +1,37 @@
+package org.example.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.entity.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class AuthenticationControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void save_shouldReturnStatusBadRequest_whenUserNameIsNull() throws Exception {
+        final User userDto = new User();
+        userDto.setName(null);
+        userDto.setPassword("12345");
+
+        mockMvc.perform(post("/gtwcl/users")
+                        .content(objectMapper.writeValueAsString(userDto))
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON))
+                .andExpect(status().is2xxSuccessful());
+    }
+
+}
